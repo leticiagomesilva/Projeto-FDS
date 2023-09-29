9791e98d37d5e86ea3a4670c53bb8e1f38be5ebf
Letícia <132513364+leticiagomesilva@users.noreply.github.com>
2023-09-29 13:01:57 -0300

    Delete exquisite directory

--- exquisite/apps/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name="index"),
-    path('ranking/', views.ranking, name="ranking"),
-    path('metas/', views.metas, name="metas"),
-]
