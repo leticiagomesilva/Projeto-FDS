39a5f538b713555834a9ed80d8aea32801e2ba22
Leticia Gomes <lgs@cesar.school>
2023-10-02 19:52:27 -0300

    ajustando requirements e settings.

--- exquisite/exquisite/settings.py
+++ exquisite/exquisite/settings.py
@@ -62,22 +62,7 @@ else:
     
 # Application definition
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zs5bpvneeng2r8w266rhjiqty-08$b4lehp2s(%$7!5zc9^8wg'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
-# Application definition
-
 INSTALLED_APPS = [
-    "apps.apps.AppsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -85,6 +70,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "whitenoise.runserver_nostatic",
+    "apps.apps.AppsConfig",
 ]
 
 MIDDLEWARE = [
@@ -157,7 +143,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
 STATIC_URL = os.environ.get('DJANGO_STATIC_URL', "/static/")
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
