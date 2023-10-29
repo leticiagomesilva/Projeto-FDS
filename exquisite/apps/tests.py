c906549f2c94f1bac5eec5e4703ed41f588a2ea2
hugorocha05 <har3@cesar.school>
2023-10-29 16:23:46 -0300

    Começo dos testes com Selenium

--- exquisite/apps/tests.py
+++ exquisite/apps/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
-# Create your tests here.
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+
+PATH = webdriver.ChromeService(executable_path="C:\Program Files (x86)\chromedriver.exe")
+driver = webdriver.Chrome(options=options, service=PATH)
+
+driver.get("https://exquisite-app.azurewebsites.net/")
+print(driver.title)
+
+
+driver.quit()
