74bad4b125df2bf3c8b36d1e9b552c1dfeb1b716
hugorocha05 <har3@cesar.school>
2023-11-26 17:53:27 -0300

    2/3 testes feitos

--- exquisite/exquisite/settings.py
+++ exquisite/exquisite/settings.py
@@ -69,7 +69,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_filters',
     "whitenoise.runserver_nostatic",
     "apps.apps.AppsConfig",
 ]
